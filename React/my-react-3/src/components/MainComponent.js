7b786e24212299807664ff37cfa043335aee35cb
unknown <sohelkhan2497@gmail.com>
2022-01-18 20:50:40 +0530

    React 3rd commit.

---
+++ React/my-react-3/src/components/MainComponent.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+  return{
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  }
+}
+
+class Main extends Component {
+
+  constructor(props){
+    super(props);
+
+  }
+
+  render() {
+    const DishWithId = ({match}) => {
+      return(
+        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+        />
+      );
+    }
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/home" component={() => <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+                                                     promotion={this.props.promotions.filter((promotion) => promotion.featured)[0]}
+                                                     leader={this.props.leaders.filter((leader) => leader.featured)[0]} />} />
+          <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(Main));
